4a05a600d6b69562513fb8a4c144e9da1b182b71
David Wheatley <hi@davwheat.dev>
2020-08-09 11:34:42 +0100

    Update footer to remove traces of giffgaff

--- src/components/Templates/Footer/index.js
+++ src/components/Templates/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import styles from './styles/footer.module.css'
+import Link from '../../Links/Link'
 
 export default function Footer() {
   return (
@@ -9,27 +10,17 @@ export default function Footer() {
         <section className={styles.about}>
           <p className="text-speak-up">&copy; {new Date().getFullYear()} David Wheatley</p>
           <p className="text-whisper">
-            <a href="https://github.com/davwheat/giffgaff-status" rel="noopener noreferrer">
+            <a href="https://github.com/davwheat/personal-portfolio" rel="noopener noreferrer">
               View this site on GitHub
             </a>
           </p>
-          <p className="text-whisper">
-            This page is run by a member of the giffgaff community and does not represent the views or opinions of giffgaff Limited, nor its
-            staff. giffgaff is the registered trademark of giffgaff Limited.
-          </p>
         </section>
         <nav className={styles.nav}>
-          <a href="https://giffgaff.com?utm_source=giffgaffstatus_com_mrjeeves" rel="noopener noreferrer">
-            giffgaff home
-          </a>
-          <a href="https://giffgaff.com/dashboard?utm_source=giffgaffstatus_com_mrjeeves" rel="noopener noreferrer">
-            My giffgaff
-          </a>
-          <a href="https://community.giffgaff.com/?utm_source=giffgaffstatus_com_mrjeeves" rel="noopener noreferrer">
-            giffgaff community
-          </a>
-          <a href="https://giffgaff.davwheat.dev/?utm_source=giffgaffstatus_com_mrjeeves" rel="noopener noreferrer">
-            giffgaff unofficial help
+          <Link href="/">Home</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/my-work">Portfolio</Link>
+          <a href="https//github.com/davwheat" rel="noopener noreferrer">
+            My GitHub
           </a>
         </nav>
       </main>
