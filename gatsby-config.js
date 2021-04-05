5d39259bc26e6cee3e41c360e5d8e235e1518377
David Wheatley <hi@davwheat.dev>
2021-04-05 02:08:50 +0100

    Add CSP plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
     `gatsby-plugin-preact`,
     `gatsby-plugin-react-head`,
     `gatsby-plugin-less`,
+    `gatsby-plugin-csp`,
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
@@ -72,5 +73,12 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }
