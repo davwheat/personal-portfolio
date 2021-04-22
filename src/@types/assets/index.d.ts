154b91ff29c087c170f6c0112abbd27040c49a0f
David Wheatley <hi@davwheat.dev>
2021-04-23 00:06:35 +0100

    Fix linting

--- src/@types/assets/index.d.ts
+++ src/@types/assets/index.d.ts
@@ -10,6 +10,7 @@ declare module '*.jpg' {
 
 declare module '*.inline.svg' {
   import { ReactElement, SVGProps } from 'react'
+
   const content: (props: SVGProps<SVGElement>) => ReactElement
   export default content
 }
