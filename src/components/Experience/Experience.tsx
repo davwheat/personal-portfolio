b2e59c585384ec79608523f82b05b3527b1269a6
David Wheatley <hi@davwheat.dev>
2021-05-04 15:20:33 +0100

    Update experience

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -7,27 +7,38 @@ const ExperienceData: ReadonlyArray<ExperienceDataEntry> = [
   {
     organisation: 'Flarum',
     organisationSite: 'https://flarum.org',
-    description: 'Free, open-source forum software',
-    role: 'a core developer trainee',
-    projects: ['Improved accessibility', 'Replaced old JS libraries with native methods', 'Pioneered CSS modernisation'],
+    description: 'Free, open-source and extensible forum framework',
+    role: 'a front-end core developer',
+    projects: [
+      'Improved accessibility',
+      'Replaced old JS libraries with newer and native methods',
+      'Pioneered CSS modernisation using grid and flexbox',
+      'Spearheaded the shift to Typescript and ES6+',
+    ],
   },
   {
     organisation: 'giffgaff',
     organisationSite: 'https://community.giffgaff.com',
-    description: 'UK mobile network',
+    description: 'UK mobile network with 1M+ members',
     role: 'a web developer',
     projects: [
-      'Fixed CSS styling issues',
-      'Created a dark theme for their community forum',
-      'Worked with developers to implement new community features',
+      'Discovered and fixed styling issues',
+      'Produced a dark theme for their community forum',
+      'Implemented new community features',
+      'Optimised stylesheets and Javascript',
     ],
   },
   {
     organisation: 'Techway Solutions',
     organisationSite: 'https://play.google.com/store/apps/details?id=dev.davwheat.flueleakagecalculator',
-    description: 'Engineering services',
+    description: 'Industrial engineering services',
     role: 'a mobile developer',
-    projects: ['Created a app used for certifying industrial flues'],
+    projects: [
+      'Created a app used for certifying industrial flues',
+      'Engaged with the client to ensure the specification is met',
+      'Provided ongoing maintenance for the app into the future',
+      'Interfaced with native APIs to implement complex features',
+    ],
   },
 ] as const
 
@@ -45,6 +56,12 @@ const useStyles = makeStyles({
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gap: 16,
+
+    width: '85vw',
+    maxWidth: 1000,
+    marginLeft: '50%',
+    transform: 'translateX(-50%)',
+
     [Breakpoints.upTo.tablet]: {
       gridTemplateColumns: '1fr',
     },
