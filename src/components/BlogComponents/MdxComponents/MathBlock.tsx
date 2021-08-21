1616d8f1caa03878ad20c02b67fa6df53e38321b
David Wheatley <hi@davwheat.dev>
2021-08-21 13:31:44 +0200

    Add support for merging math blocks and custom math block title colours

--- src/components/BlogComponents/MdxComponents/MathBlock.tsx
+++ src/components/BlogComponents/MdxComponents/MathBlock.tsx
@@ -10,21 +10,42 @@ interface IMathBlockProps {
   title?: string
   math: string
   footer?: string
+  color: 'blue' | 'pink' | 'neutral'
 }
 
 const useStyles = makeStyles({
   root: {
     marginTop: '1em',
     marginBottom: '1em',
+
+    'figure[role="group"] &': {
+      marginBottom: 0,
+      marginTop: 0,
+
+      '&:first-child': {
+        marginTop: '1em',
+      },
+      '&:last-child': {
+        marginBottom: '1em',
+      },
+    },
   },
   title: {
     fontWeight: 'bold',
     padding: '8px 16px',
+  },
+  title_blue: {
     background: Colors.primaryBlue,
+  },
+  title_pink: {
+    background: Colors.primaryRed,
+  },
+  title_neutral: {
+    background: Colors.neutralGrey,
   },
   mathBlock: {
     background: Colors.lightGrey,
-    padding: 8,
+    padding: '8px 16px',
     overflowX: 'auto',
   },
   warning: {
@@ -51,12 +72,12 @@ const useStyles = makeStyles({
   },
 })
 
-export function MathBlock({ title, math, footer }: IMathBlockProps) {
+export function MathBlock({ title, math, footer, color = 'blue' }: IMathBlockProps) {
   const classes = useStyles()
 
   return (
     <figure className={classes.root}>
-      {title && <figcaption className={classes.title}>{title}</figcaption>}
+      {title && <figcaption className={clsx(classes.title, classes[`title_${color}`])}>{title}</figcaption>}
 
       <TeX className={classes.mathBlock} block math={math} />
 
