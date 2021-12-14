bb96074f286e6d311a1c9ccc01c01f53733aef76
David Wheatley <hi@davwheat.dev>
2021-12-14 01:06:49 +0000

    wip: add orion article

---
+++ src/data/SpectrumAllocation/Bands/40.tsx
@@ -0,0 +1,26 @@
+import type { ISpectrumAllocation } from '@components/BlogComponents/MdxComponents'
+
+const Band40: ISpectrumAllocation[] = [
+  {
+    owner: 'MoD',
+    ownerLongName: 'Ministry of Defence',
+    freqStart: 2300,
+    freqEnd: 2350,
+    type: 'unknown',
+  },
+  {
+    owner: 'O2',
+    freqStart: 2350,
+    freqEnd: 2390,
+    type: 'tdd',
+  },
+  {
+    owner: 'MoD',
+    ownerLongName: 'Ministry of Defence',
+    freqStart: 2390,
+    freqEnd: 2400,
+    type: 'unknown',
+  },
+]
+
+export default Band40
