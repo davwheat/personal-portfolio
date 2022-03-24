c6a29fd14dd2de871eaba01c5e126db34ef11664
David Wheatley <hi@davwheat.dev>
2022-03-24 00:23:36 +0000

    chore: update ARFCN list intro copy

--- src/pages/mobile-networking/uk/arfcn-list.tsx
+++ src/pages/mobile-networking/uk/arfcn-list.tsx
@@ -285,14 +285,20 @@ function MobileNetworkingPage({ location }) {
 
         <p className="text-speak">
           An <strong>ARFCN</strong> is an acronym which stands for absolute radio frequency channel number. This is a unique number which can
-          represent any valid radio frequency covered by the appropriate 3GPP radio access technology (RAT) specification.
+          represent any valid radio frequency covered by the appropriate 3GPP radio access technology (RAT)&nbsp;specification.
         </p>
-        <p>
-          ARFCN can be prefixed with characters to show which RAT they are representing. NRARFCNs are for 5G/
+
+        <p className="text-speak">
+          ARFCN can be prefixed with characters to show which RAT they are representing. NR ARFCNs are for 5G/
           <abbr data-tooltip aria-label="New Radio">
             NR
           </abbr>
-          , EARFCNs are for EUTRA (4G/LTE), UARFCNs are for UMTS (3G) and ARFCNs are for GSM (2G).
+          , EARFCNs are for EUTRA (4G/LTE), UARFCNs are for UMTS (3G) and ARFCNs are for GSM&nbsp;(2G).
+        </p>
+
+        <p className="text-speak">
+          In 4G LTE and previous generations of RAT, ARFCNs represented the centre of a carrier band, but with 5G NR this changed to the NR ARFCN
+          representing any frequency within the network's carrier, and the centre point is often determined using an offset sent in the&nbsp;SIB1.
         </p>
       </Section>
 
